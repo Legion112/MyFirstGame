d195ef2a06846d6a7b8a54665a6037ed70169834
Max Ageev <ageev.maks2014@yandex.ru>
2017-04-30 02:33:53 +0500

    I create the new class StartGame. This class start game.

---
+++ src/ru/fearofcode/StartGame.java
@@ -0,0 +1,12 @@
+package ru.fearofcode;
+
+import javax.swing.*;
+
+/**
+ * Created by maks on 30.04.2017.
+ */
+public class StartGame {
+    public static void main(String[] args) {
+        GameFrame gameFrame = new GameFrame("Shooter 2D", new GamePanel());
+    }
+}
